fa81e750b4a6589456beb17fe2b72256e4a9a9a6
xiii_1991 <xiii_1991@163.com>
2017-02-04 03:40:24 +0800

    detector ok

---
+++ src/main/scala/candylib/functions/CodeDetect.scala
@@ -0,0 +1,68 @@
+package candylib.functions
+
+import java.io.{BufferedInputStream, File, FileInputStream}
+
+import org.mozilla.intl.chardet.nsDetector
+import org.mozilla.intl.chardet.nsICharsetDetectionObserver
+
+import scala.collection.mutable.ArrayBuffer
+
+class CodeDetect(file:File) {
+  val inputFile = file
+  
+  private def parse(detector:nsDetector):Seq[String] = {
+    val encodings = ArrayBuffer[String]()
+    var found = false
+    detector.Init(new nsICharsetDetectionObserver{
+      override def Notify(s:String) = {
+        encodings += s
+        found = true
+      }
+    })
+
+    val imp = new BufferedInputStream(new FileInputStream((inputFile)))
+    val buf = new Array[Byte](1024)
+    var len:Int = 0
+    var done:Boolean = false
+    var isASCII:Boolean = false
+    do {
+      len = imp.read(buf, 0, buf.length)
+      isASCII = detector.isAscii(buf, len)
+      done = detector.DoIt(buf, len, false)
+    }while((len != -1) || isASCII || done)
+    imp.close()
+    detector.DataEnd()
+
+    if (isASCII){
+      Seq[String]("ASCII")
+    }else{
+      detector.getProbableCharsets.toSeq
+    }
+  }
+
+  /**
+    * 猜测文本文件编码方式
+    *
+    * @param languageHint 地区语言提示代码，取值如下
+    *                    - 1 : Japanese
+    *                    - 2 : Chinese
+    *                    - 3 : Simplified Chinese
+    *                    - 4 : Traditional Chinese
+    *                    - 5 : Korean
+    *                    - 6 : Don't know(default)
+    * @return 猜测的编码的序列，按照可能性排列
+    */
+  def guessEncoding(languageHint:Int):Seq[String] = {
+    parse(new nsDetector(languageHint))
+  }
+
+  def guessEncoding():Seq[String] = parse(new nsDetector())
+
+}
+
+object CodeDetect{
+  def apply(file: File): CodeDetect = new CodeDetect(file)
+  def quickGuess(file:File):String = {
+    new CodeDetect(file).guessEncoding()(0)
+  }
+}
