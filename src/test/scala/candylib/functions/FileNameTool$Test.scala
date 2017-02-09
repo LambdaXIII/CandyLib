e1ed073f8bc6c60343989be561dfef902eb8f0e9
xiii_1991 <xiii_1991@163.com>
2017-02-10 06:42:52 +0800

    update test

--- src/test/scala/candylib/functions/FileNameTool$Test.scala
+++ src/test/scala/candylib/functions/FileNameTool$Test.scala
@@ -7,9 +7,17 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class FileNameTool$Test extends FlatSpec with Matchers {
   it should "init properly in different os." in {
-    val a = FileNameTool("d:\\q\\d\\a.b")
-    a.pureDir should be("d:\\q\\d\\")
-    a.pureName should be("a")
-    a.pureExt should be(".b")
+    System.getProperty("os.name") match {
+      case m: String if (m.toLowerCase().contains("windows")) =>
+        val a = FileNameTool("d:\\q\\d\\a.b")
+        a.pureDir should be("d:\\q\\d\\")
+        a.pureName should be("a")
+        a.pureExt should be(".b")
+      case m: String if (m.toLowerCase().contains("linux")) =>
+        val a = FileNameTool("/d/q/d/a.b")
+        a.pureDir should be("/d/q/d/")
+        a.pureName should be("a")
+        a.pureExt should be(".b")
+    }
   }
 }
