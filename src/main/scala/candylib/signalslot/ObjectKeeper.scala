3ba4acea7f419db4a13e655a4e9079703ecd61ef
xiii_1991 <xiii_1991@163.com>
2017-04-13 18:21:02 +0800

    Object keeper

---
+++ src/main/scala/candylib/signalslot/ObjectKeeper.scala
@@ -0,0 +1,22 @@
+package candylib.signalslot
+
+/**
+  * Created by a on 2017/4/13.
+  */
+class ObjectKeeper[T](o:T) {
+  val editStatusChangedSignal = new SimpleSignal[Boolean]("Treasure Edited.")
+
+  private val _t:T = o
+  def treasure:T = _t
+
+  private var _edited = false
+  def edited:Boolean = _edited
+  def edited_=(v:Boolean):Unit = {
+    _edited = v
+    editStatusChangedSignal.emit(_edited)
+  }
+
+  def setSaved():Unit = edited = false
+  def isSaved():Boolean = ! edited
+
+}
