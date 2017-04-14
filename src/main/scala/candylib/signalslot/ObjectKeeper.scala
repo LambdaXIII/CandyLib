fef252f2459c7bec0268ea461893a6edb22c127f
xiii_1991 <xiii_1991@163.com>
2017-04-14 23:01:22 +0800

    bug fix

--- src/main/scala/candylib/signalslot/ObjectKeeper.scala
+++ src/main/scala/candylib/signalslot/ObjectKeeper.scala
@@ -3,25 +3,34 @@ package candylib.signalslot
 /**
   * Created by a on 2017/4/13.
   */
-class ObjectKeeper[T](o:T) {
+class ObjectKeeper[T]() {
   val editStatusChangedSignal = new SimpleSignal[Boolean]("Treasure Edited.")
 
-  private val _t:T = o
-  def treasure:T = _t
+  private var _t: T = _
+
+  def treasure: T = _t
+
+  def treasure_=(v: T): Unit = _t = v
+
+  def this(o: T) = this() {
+    treasure = o
+  }
 
   private var _edited = false
-  def edited:Boolean = _edited
-  def edited_=(v:Boolean):Unit = {
+
+  def edited: Boolean = _edited
+
+  def edited_=(v: Boolean): Unit = {
     _edited = v
     editStatusChangedSignal.emit(_edited)
   }
 
-  def setSaved():Unit = edited = false
-  def isSaved():Boolean = ! edited
+  def setSaved(): Unit = edited = false
+
+  def isSaved(): Boolean = !edited
 
 }
 
-object ObjectKeeper
-{
+object ObjectKeeper {
   def apply[T](o: T): ObjectKeeper[T] = new ObjectKeeper(o)
 }
