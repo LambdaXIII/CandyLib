11b0fc21a8506bc8ccadf0a7778ea88447d6c1fe
xiii_1991 <xiii_1991@163.com>
2017-04-15 00:31:20 +0800

    rearranged code and add doc

--- src/main/scala/candylib/functions/FileFunctions.scala
+++ src/main/scala/candylib/functions/FileFunctions.scala
@@ -6,15 +6,26 @@ import scala.collection.mutable
 
 /**
   * Created by a on 2017/3/30.
+  *
+  * A bunch of pure functions to deal with [[java.io.File]] objects
+  * or files on file system.
+  *
   */
 object FileFunctions {
-  def expandDirs(files:Set[File]):Set[File] = {
+  /**
+    * Expand Dirs to a [[mutable.Set]] of [[java.io.File]] in place.
+    * @param files a [[mutable.Set]] of [[java.io.File]].
+    *              Some of the Files may be a Directory.
+    * @return a Set of Files, all Directories in the input set will
+    *         expand to a lot of files in place.
+    */
+  def expandDirs(files: Set[File]): Set[File] = {
     val expandedFiles = mutable.HashSet[File]()
     files.foreach((f) => {
-      if(f.exists()){
-        if (f.isDirectory){
+      if (f.exists()) {
+        if (f.isDirectory) {
           expandedFiles ++= expandDirs(f.listFiles().toSet)
-        }else{
+        } else {
           expandedFiles += f
         }
       }
@@ -22,14 +33,26 @@ object FileFunctions {
     expandedFiles.toSet
   }
 
-  def getFileSize(file:File): Long =
-    if (file.exists()){
+  /**
+    * Calculate the size of a [[java.io.File]]
+    * @param file the File
+    * @return a Long Integer size of the file.
+    *         Will be 0 if the file isn't exist.
+    */
+  def getFileSize(file: File): Long =
+    if (file.exists()) {
       val inStream = new FileInputStream(file)
       inStream.available()
-    }else 0
+    } else 0
 
-  def saveTextFile(outputFile:File, content:String, encoding:String = "UTF-8"): Unit ={
-    if (! outputFile.exists()) outputFile.createNewFile()
+  /**
+    * A quick function to save a String to a file.
+    * @param outputFile Target File
+    * @param content the String you want to save.
+    * @param encoding the Encoding of the file.
+    */
+  def saveTextFile(outputFile: File, content: String, encoding: String = "UTF-8"): Unit = {
+    if (!outputFile.exists()) outputFile.createNewFile()
 
     val writer = new BufferedWriter(
       new OutputStreamWriter(
@@ -43,7 +66,7 @@ object FileFunctions {
     writer.close()
   }
 
-  def saveTextFile(filename:String, content:String, encoding:String):Unit =
+  def saveTextFile(filename: String, content: String, encoding: String): Unit =
     saveTextFile(new File(filename), content, encoding)
 
 }
